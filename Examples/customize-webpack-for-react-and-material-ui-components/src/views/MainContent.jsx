3b2932dec2822150858abe95fc74ac23a4cbb5d7
ubbcou <ubbcou@outlook.com>
2019-01-08 16:55:45 +0800

    delete something

--- Examples/customize-webpack-for-react-and-material-ui-components/src/views/MainContent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '@/assets/views/mainContent.css';
-import Paper from '@/components/Paper.jsx';
-
-export default () => (
-  <div className="app-main-content">
-    <Paper>paper</Paper>
-  </div>
-)
